c9cb76199682349e9737e7590e305fae0396e0cf
Jester565 <ajcraig543@gmail.com>
2023-10-09 20:56:22 -0700

    fix shifting grid

--- structures/ShiftingGrid.gd
+++ structures/ShiftingGrid.gd
@@ -3,47 +3,152 @@ class_name ShiftingGrid
 const GridElement = preload("res://structures/GridElement.gd")
 
 var _GridElement
-var _grid: CircularBuffer
-var _idx_rect: Rect2i
-var _buffer_width = 0
-var _buffer_height = 0
-var _max_idx_rect: Rect2i
+var _grid: CircularBuffer = null
+var _bounds
+var _max_bounds
+var _buffer_dimensions
 
-func _init(GridElementClass, init_idx_rect: Rect2i, max_idx_rect: Rect2i):
+func _init(GridElementClass, position: Vector2i, buffer_dimensions: Vector2i, max_bounds: Rect2i):
     self._GridElement = GridElementClass
-    self._max_idx_rect = max_idx_rect
-    self._buffer_width = init_idx_rect.size.x + 1
-    self._buffer_height = init_idx_rect.size.y + 1
-    var contained_idx_rect = max_idx_rect.intersection(init_idx_rect)
-    self._idx_rect = contained_idx_rect
-    self._grid = CircularBuffer.new([], contained_idx_rect.position.x, self._buffer_width)
-    
-    for i in range(contained_idx_rect.position.x, contained_idx_rect.end.x + 1):
-        var col = CircularBuffer.new([], contained_idx_rect.position.y, self._buffer_height)
-        for j in range(contained_idx_rect.position.y, contained_idx_rect.end.y + 1):
-            col.push_to_end(GridElement.new(i, j))
-        self._grid.push_to_end(col)
+    self._max_bounds = max_bounds
+    self._buffer_dimensions = buffer_dimensions
+    var uncontained_bounds = Rect2i(position, buffer_dimensions)
+    self._bounds = max_bounds.intersection(uncontained_bounds)
+    if self._bounds.has_area():
+        self._init_grid(self._bounds, uncontained_bounds)
 
-func shift(new_idx_rect: Rect2i):
-    var new_contained_idx_rect = self._max_idx_rect.intersection(new_idx_rect)
-    for j in range(self._idx_rect.end.y + 1, new_contained_idx_rect.end.y + 1):
-        for i in range(self._idx_rect.position.x, self._idx_rect.end.x + 1):
-            var col: CircularBuffer = self._grid.get_at(i)
-            col.push_to_end(self._GridElement.new(i, j))
-    for j in range(new_contained_idx_rect.position.y, self._idx_rect.position.y):
-        for i in range(self._idx_rect.position.x, self._idx_rect.end.x + 1):
-            var col: CircularBuffer = self._grid.get_at(i)
-            col.push_to_start(self._GridElement.new(i, j))
-    for i in range(self._idx_rect.end.x + 1, new_contained_idx_rect.end.x + 1):
-        var col = CircularBuffer.new([], new_contained_idx_rect.position.y, self._buffer_height)
-        for j in range(new_contained_idx_rect.position.y, new_contained_idx_rect.end.y + 1):
-            col.push_to_end(self._GridElement.new(i, j))
-        self._grid.push_to_end(col)
-    for i in range(new_contained_idx_rect.position.x, self._idx_rect.position.x):
-        var col = CircularBuffer.new([], new_contained_idx_rect.position.y, self._buffer_height)
-        for j in range(new_contained_idx_rect.position.y, new_contained_idx_rect.end.y + 1):
-            col.push_to_end(self._GridElement.new(i, j))
-        self._grid.push_to_start(col)
+func move_to(position: Vector2i):
+    var new_uncontained_bounds = Rect2i(position, self._buffer_dimensions)
+    var new_bounds = self._max_bounds.intersection(new_uncontained_bounds)
+    if !self._bounds.has_area() and new_bounds.has_area():
+        self._init_grid(new_bounds, new_uncontained_bounds)
+    elif self._bounds.has_area() and !new_bounds.has_area():
+        self._clear_grid()
+    elif self._bounds.has_area() and new_bounds.has_area():
+        # only shift if there's some overlap
+        if self._bounds.intersects(new_bounds):
+            self._shift(new_bounds, new_uncontained_bounds)
+        else:
+            self._clear_grid()
+            self._init_grid(new_bounds, new_uncontained_bounds)
+    self._bounds = new_bounds
 
-func get_at(i: int, j: int) -> GridElement:
+func get_at(i: int, j: int):
     return self._grid.get_at(i).get_at(j)
+
+func get_at_indexes(indexes: Vector2i):
+    return self._grid.get_at(indexes.x).get_at(indexes.y)
+
+func get_dimensions():
+    assert(
+        (self._bounds.size.x == 0 and self._grid == null) or (self._bounds.size.x == self._grid.get_size()), 
+        "Size of bounds and grid don't match: BoundsWidth=%s, GridSize=%s" % [self._bounds.size, self._grid.get_size() if self._grid != null else 0]
+    )
+    assert(
+        (self._bounds.size.y == 0 and self._grid == null) or (self._bounds.size.y == self._grid.get_at(self._bounds.position.x).get_size()), 
+        "Size of bounds and grid don't match: BoundsHeight=%s, ColSize=%s" % [self._bounds.size, self._grid.get_at(self._bounds.position.x).get_size() if self._grid != null else 0]
+    )
+    return self._bounds.size
+
+func for_each(callable: Callable):
+    if self._grid != null:
+        var col_callable = func(col, i):
+            var elm_callable = func(elm, j):
+                callable.call(elm, Vector2i(i, j))
+            col.for_each(elm_callable)
+        self._grid.for_each(col_callable)
+
+func get_bounds():
+    return self._bounds
+
+func _shift(new_bounds: Rect2i, _new_uncontained_bounds: Rect2i):
+    for j in self._get_new_top_row_indexes(self._bounds, new_bounds):
+        self._push_top_row(self._bounds.position.x, self._bounds.size.x)
+    for j in self._get_new_bottom_row_indexes(self._bounds, new_bounds):
+        self._push_bottom_row(self._bounds.position.x, self._bounds.size.x)
+    
+    for i in self._get_new_left_col_indexes(self._bounds, new_bounds):
+        self._push_left_col(new_bounds.position.y, new_bounds.size.y)
+    for i in self._get_new_right_col_indexes(self._bounds, new_bounds):
+        self._push_right_col(new_bounds.position.y, new_bounds.size.y)
+    
+    self._remove_out_of_bounds_elements(new_bounds)
+    
+func _remove_out_of_bounds_elements(new_bounds):
+    var col = self._grid.get_at(self._grid.get_start_i())
+    var current_rect = Rect2i(self._grid.get_start_i(), col.get_start_i(), self._grid.get_size(), col.get_size())
+    
+    for j in range(new_bounds.size.y, current_rect.size.y):
+        # top is moving down but the size is shrinking, so remove from top
+        if new_bounds.position.y != current_rect.position.y:
+            self._pop_top_row() 
+        else:
+            self._pop_bottom_row()
+    for i in range(new_bounds.size.x, current_rect.size.x):
+        if new_bounds.position.x != current_rect.position.x:
+            self._pop_left_col()
+        else:
+            self._pop_right_col()
+
+func _init_grid(bounds, _uncontained_bounds):
+    self._grid = CircularBuffer.new([], bounds.position.x, self._buffer_dimensions.x)
+
+    for i in range(bounds.position.x, bounds.end.x):
+        var col = CircularBuffer.new([], bounds.position.y, self._buffer_dimensions.y)
+        for j in range(bounds.position.y, bounds.end.y):
+            col.push_to_end(self._GridElement.new(i, j))
+        self._grid.push_to_end(col)
+
+func _clear_grid():
+    self._grid = null
+    self._bounds = Rect2i(0, 0, 0, 0)
+
+func _get_new_bottom_row_indexes(prev_bounds: Rect2i, new_bounds: Rect2i):
+    return range(prev_bounds.end.y, new_bounds.end.y)
+
+func _get_new_top_row_indexes(prev_bounds: Rect2i, new_bounds: Rect2i):
+   return range(new_bounds.position.y, prev_bounds.position.y)
+
+func _get_new_left_col_indexes(prev_bounds: Rect2i, new_bounds: Rect2i):
+    return range(new_bounds.position.x, prev_bounds.position.x)
+
+func _get_new_right_col_indexes(prev_bounds: Rect2i, new_bounds: Rect2i):
+    return range(prev_bounds.end.x, new_bounds.end.x)
+
+func _push_top_row(start_i, width):
+    for i in range(start_i, start_i + width):
+        var col: CircularBuffer = self._grid.get_at(i)
+        col.push_to_start(self._GridElement.new(i, col.get_start_i() - 1))
+
+func _push_bottom_row(start_i, width):
+    for i in range(start_i, start_i + width):
+        var col: CircularBuffer = self._grid.get_at(i)
+        col.push_to_end(self._GridElement.new(i, col.get_start_i() + col.get_size()))
+
+func _push_left_col(start_j, height):
+    var col = CircularBuffer.new([], start_j, self._buffer_dimensions.y)
+    for j in range(start_j, start_j + height):
+        col.push_to_end(self._GridElement.new(self._grid.get_start_i() - 1, j))
+    self._grid.push_to_start(col)
+
+func _push_right_col(start_j, height):
+    var col = CircularBuffer.new([], start_j, self._buffer_dimensions.y)
+    for j in range(start_j, start_j + height):
+        col.push_to_end(self._GridElement.new(self._grid.get_start_i() + self._grid.get_size(), j))
+    self._grid.push_to_end(col)
+
+func _pop_top_row():
+    for i in range(self._grid.get_start_i(), self._grid.get_size()):
+        var col: CircularBuffer = self._grid.get_at(i)
+        col.pop_from_start()
+
+func _pop_bottom_row():
+    for i in range(self._grid.get_start_i(), self._grid.get_size()):
+        var col: CircularBuffer = self._grid.get_at(i)
+        col.pop_from_end()
+
+func _pop_left_col():
+    self._grid.pop_from_start()
+
+func _pop_right_col():
+    self._grid.pop_from_end()
--- test/unit/structures/test_ShiftingGrid.gd
+++ test/unit/structures/test_ShiftingGrid.gd
@@ -3,60 +3,84 @@ extends GutTest
 const GridElement = preload("res://structures/GridElement.gd")
 const ShiftingGrid = preload("res://structures/ShiftingGrid.gd")
 
-var shifting_grid: ShiftingGrid
-
-func before_each():
-    shifting_grid = ShiftingGrid.new(GridElement, Rect2i(0, 0, 3, 3), Rect2i(-1, -1, 6, 5))
+var shifting_grid
 
 func test_init_within_max():
-    shifting_grid = ShiftingGrid.new(GridElement, Rect2i(0, 0, 3, 3), Rect2i(1, 1, 2, 1))
-    var top_left_elm = shifting_grid.get_at(1, 1)
-    assert_eq(top_left_elm.get_i(), 1)
-    assert_eq(top_left_elm.get_j(), 1)
-
-    var bottom_right_elm = shifting_grid.get_at(3, 2)
-    assert_eq(bottom_right_elm.get_i(), 3)
-    assert_eq(bottom_right_elm.get_j(), 2)
+    _set_shifting_grid(Vector2i(0, 0), Vector2i(4, 4), Rect2i(1, 1, 3, 2))
+    _verify_indexes_match(Rect2i(1, 1, 3, 2))
 
 func test_shift_right_by_one():
-    shifting_grid.shift(Rect2i(1, 0, 3, 3))
-    var bottom_right_elm = shifting_grid.get_at(4, 3)
-    assert_eq(bottom_right_elm.get_i(), 4)
-    assert_eq(bottom_right_elm.get_j(), 3)
+    _set_shifting_grid()
+    _verify_indexes_match(Rect2i(0, 0, 4, 4))
+    shifting_grid.move_to(Vector2i(1, 0))
+    _verify_indexes_match(Rect2i(1, 0, 4, 4))
 
 func test_shift_left_by_one():
-    shifting_grid.shift(Rect2i(-1, 0, 3, 3))
-    var left_elm = shifting_grid.get_at(-1, 2)
-    assert_eq(left_elm.get_i(), -1)
-    assert_eq(left_elm.get_j(), 2)
+    _set_shifting_grid()
+    shifting_grid.move_to(Vector2i(-1, 0))
+    _verify_indexes_match(Rect2i(-1, 0, 4, 4))
     
 func test_shift_up_by_one():
-    shifting_grid.shift(Rect2i(0, -1, 3, 3))
-    var top_right_elm = shifting_grid.get_at(3, -1)
-    assert_eq(top_right_elm.get_i(), 3)
-    assert_eq(top_right_elm.get_j(), -1)
+    _set_shifting_grid()
+    shifting_grid.move_to(Vector2i(0, -1))
+    _verify_indexes_match(Rect2i(0, -1, 4, 4))
 
 func test_shift_down_by_one():
-    shifting_grid.shift(Rect2i(0, 1, 3, 3))
-    var bottom_left_elm = shifting_grid.get_at(0, 4)
-    assert_eq(bottom_left_elm.get_i(), 0)
-    assert_eq(bottom_left_elm.get_j(), 4)
+    _set_shifting_grid()
+    shifting_grid.move_to(Vector2i(0, 1))
+    _verify_indexes_match(Rect2i(0, 1, 4, 4))
 
 func test_shifting_left_in_unfilled():
-    shifting_grid = ShiftingGrid.new(GridElement, Rect2i(0, 0, 3, 3), Rect2i(1, 1, 2, 2))
-    shifting_grid.shift(Rect2i(-1, 0, 3, 3))
-    var bottom_right_elm = shifting_grid.get_at(3, 3)
-    assert_eq(bottom_right_elm.get_i(), 3)
-    assert_eq(bottom_right_elm.get_j(), 3)
-    shifting_grid.shift(Rect2i(2, 0, 3, 3))
-    var bottom_left_elm = shifting_grid.get_at(2, 3)
-    assert_eq(bottom_left_elm.get_i(), 2)
-    assert_eq(bottom_left_elm.get_j(), 3)
+    _set_shifting_grid(Vector2i(0, 0), Vector2i(4, 4), Rect2i(1, 1, 3, 3))
+    _verify_indexes_match(Rect2i(1, 1, 3, 3))
+    shifting_grid.move_to(Vector2i(-1, 0))
+    _verify_indexes_match(Rect2i(1, 1, 2, 3))
+    shifting_grid.move_to(Vector2i(3, 2))
+    _verify_indexes_match(Rect2i(3, 2, 1, 2))
+
+func test_out_of_bounds_movement():
+    _set_shifting_grid(Vector2i(-4, -4), Vector2i(4, 4), Rect2i(1, 1, 3, 3))
+    _verify_indexes_match(Rect2i(0, 0, 0, 0))
+    shifting_grid.move_to(Vector2i(-3, -3))
+    _verify_indexes_match(Rect2i(0, 0, 0, 0))
+    shifting_grid.move_to(Vector2i(-2, -2))
+    _verify_indexes_match(Rect2i(1, 1, 1, 1))
+    shifting_grid.move_to(Vector2i(0, 0))
+    _verify_indexes_match(Rect2i(1, 1, 3, 3))
+    shifting_grid.move_to(Vector2i(4, -2))
+    _verify_indexes_match(Rect2i(0, 0, 0, 0))
+    shifting_grid.move_to(Vector2i(3, 3))
+    _verify_indexes_match(Rect2i(3, 3, 1, 1))
+    shifting_grid.move_to(Vector2i(2, 2))
+    _verify_indexes_match(Rect2i(2, 2, 2, 2))
 
 func test_row():
-    shifting_grid = ShiftingGrid.new(GridElement, Rect2i(0, 0, 3, 3), Rect2i(-1, 0, 4, 1))
-    shifting_grid.shift(Rect2i(-1, -2, 3, 3))
-    var elm = shifting_grid.get_at(-1, 0)
-    assert_eq(elm.get_i(), -1)
-    assert_eq(elm.get_j(), 0)
-    shifting_grid.get_at(2, 1)
+    _set_shifting_grid(Vector2i(0, 0), Vector2i(4, 4), Rect2i(-1, 0, 5, 1))
+    shifting_grid.move_to(Vector2i(-1, -2))
+    _verify_indexes_match(Rect2i(-1, 0, 4, 1))
+    shifting_grid.move_to(Vector2i(0, -1))
+    _verify_indexes_match(Rect2i(0, 0, 4, 1))
+    shifting_grid.move_to(Vector2i(1, -2))
+    _verify_indexes_match(Rect2i(1, 0, 3, 1))
+    shifting_grid.move_to(Vector2i(2, 0))
+    _verify_indexes_match(Rect2i(2, 0, 2, 1))
+    shifting_grid.move_to(Vector2i(3, 0))
+    _verify_indexes_match(Rect2i(3, 0, 1, 1))
+
+func _set_shifting_grid(position = Vector2i(0, 0), buffer_dimensions=Vector2i(4, 4), max_bounds=Rect2i(-1, -1, 7, 6)):
+    shifting_grid = ShiftingGrid.new(GridElement, position, buffer_dimensions, max_bounds)
+
+func _verify_indexes_match(expected_bounds: Rect2i):
+    assert_eq(shifting_grid.get_dimensions(), Vector2i(expected_bounds.size.x, expected_bounds.size.y))
+    var indexes = []
+    var add_element_to_indexes = func(elm, pos):
+        assert_eq(elm.get_i(), pos.x)
+        assert_eq(elm.get_j(), pos.y)
+        indexes.append(pos)
+    shifting_grid.for_each(add_element_to_indexes)
+    assert_eq(indexes.size(), expected_bounds.get_area())
+    var arr_i = 0
+    for i in range(expected_bounds.position.x, expected_bounds.end.x):
+        for j in range(expected_bounds.position.y, expected_bounds.end.y):
+            assert_eq(indexes[arr_i], Vector2i(i, j))
+            arr_i += 1
