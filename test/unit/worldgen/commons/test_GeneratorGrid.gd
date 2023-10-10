a4c539a26a5f926c640342a9644f54c79b0eb894
Jester565 <ajcraig543@gmail.com>
2023-10-09 20:57:38 -0700

    add generator grid

---
+++ test/unit/worldgen/commons/test_GeneratorGrid.gd
@@ -0,0 +1,94 @@
+extends GutTest
+
+const Generator = preload("res://worldgen/generators/commons/Generator.gd")
+const GeneratorGrid = preload("res://worldgen/generators/commons/GeneratorGrid.gd")
+
+var DoubleGenerator
+var generator_grid: GeneratorGrid
+
+func before_each():
+    stub(Generator, 'generate').to_do_nothing()
+    DoubleGenerator = double(Generator)
+
+func test_init():
+    _set_generator_grid()
+    _verify_generate_only_called_in_bounds(Rect2i(1, 1, 2, 2))
+    # check neighbor args
+    var generator = generator_grid.get_at(2, 2)
+    var neighbors = get_call_parameters(generator, 'generate')[0]
+    for i in 3:
+        for j in 3:
+            assert_eq(neighbors[i][j], generator_grid.get_at(i + 1, j + 1))
+
+func test_shift_right_by_one():
+    _set_generator_grid()
+    generator_grid.move_to(Vector2i(1, 0))
+    _verify_generate_only_called_in_bounds(Rect2i(1, 1, 3, 2))
+    # check neighbor args
+    var generator = generator_grid.get_at(3, 2)
+    var neighbors = get_call_parameters(generator, 'generate')[0]
+    for i in 3:
+        for j in 3:
+            assert_eq(neighbors[i][j], generator_grid.get_at(i + 2, j + 1))
+
+func test_shift_left_by_one():
+    _set_generator_grid()
+    generator_grid.move_to(Vector2i(-1, 0))
+    _verify_generate_only_called_in_bounds(Rect2i(0, 1, 3, 2))
+    # check neighbor args
+    var generator = generator_grid.get_at(0, 2)
+    var neighbors = get_call_parameters(generator, 'generate')[0]
+    for i in 3:
+        for j in 3:
+            assert_eq(neighbors[i][j], generator_grid.get_at(i - 1, j + 1))
+
+func test_shift_down_by_one():
+    _set_generator_grid()
+    generator_grid.move_to(Vector2i(0, 1))
+    _verify_generate_only_called_in_bounds(Rect2i(1, 1, 2, 3))
+    # check neighbor args
+    var generator = generator_grid.get_at(2, 3)
+    var neighbors = get_call_parameters(generator, 'generate')[0]
+    for i in 3:
+        for j in 3:
+            assert_eq(neighbors[i][j], generator_grid.get_at(i + 1, j + 2))
+
+func test_shift_up_by_one():
+    _set_generator_grid()
+    generator_grid.move_to(Vector2i(0, -1))
+    _verify_generate_only_called_in_bounds(Rect2i(1, 0, 2, 3))
+    # check neighbor args
+    var generator = generator_grid.get_at(2, 0)
+    var neighbors = get_call_parameters(generator, 'generate')[0]
+    for i in 3:
+        for j in 3:
+            assert_eq(neighbors[i][j], generator_grid.get_at(i + 1, j - 1))
+
+func test_row():
+    _set_generator_grid(Vector2i(0, 0), Vector2i(4, 4), Rect2i(-1, 0, 4, 1), 1, 0)
+    generator_grid.move_to(Vector2i(-2, -1))
+    _verify_generate_only_called_in_bounds(Rect2i(-1, 0, 4, 1))
+
+func test_out_of_bounds_movement():
+    _set_generator_grid(Vector2i(-4, -4), Vector2i(4, 4), Rect2i(1, 1, 3, 3))
+    generator_grid.move_to(Vector2i(-3, -3))
+    generator_grid.move_to(Vector2i(-2, -2))
+    generator_grid.move_to(Vector2i(0, 0))
+    generator_grid.move_to(Vector2i(4, -2))
+    generator_grid.move_to(Vector2i(3, 3))
+    generator_grid.move_to(Vector2i(2, 2))
+    for i in range(2, 3):
+        for j in range(2, 3):
+            var generator = generator_grid.get_at(i, j)
+            assert_not_called(generator, 'generate')
+
+func _set_generator_grid(pos=Vector2i(0, 0), buf_dims=Vector2i(4, 4), max_bounds=Rect2i(-2, -2, 9, 9), dist_x_sides=1, dist_y_sides=1):
+    generator_grid = GeneratorGrid.new(DoubleGenerator, pos, buf_dims, max_bounds, dist_x_sides, dist_y_sides)
+
+func _verify_generate_only_called_in_bounds(generate_bounds: Rect2i):
+    var check_generate_call = func(generator, pos):
+        if generate_bounds.has_point(pos):
+            assert_call_count(generator, 'generate', 1)
+        else:
+            assert_not_called(generator, 'generate')
+    generator_grid.for_each(check_generate_call)
---
+++ worldgen/generators/commons/GeneratorGrid.gd
@@ -0,0 +1,100 @@
+extends ShiftingGrid
+
+var _Generator
+var _generate_bounds: Rect2i
+var _generate_at_distance_from_x_sides
+var _generate_at_distance_from_y_sides
+
+func _init(GeneratorClass, position: Vector2i, buffer_dimensions: Vector2i, max_bounds: Rect2i, generate_at_distance_from_x_sides=1, generate_at_distance_from_y_sides=1):
+    self._Generator = GeneratorClass
+    self._generate_at_distance_from_x_sides = generate_at_distance_from_x_sides
+    self._generate_at_distance_from_y_sides = generate_at_distance_from_y_sides
+    super(GeneratorClass, position, buffer_dimensions, max_bounds)
+    
+func _init_grid(bounds, uncontained_bounds):
+    super(bounds, uncontained_bounds)
+    self._run_generate_on_bounds(uncontained_bounds)
+
+func _run_generate_on_bounds(uncontained_bounds):
+    var generate_bounds = self._get_generate_bounds(uncontained_bounds)
+    self._generate_bounds = generate_bounds
+    for j in range(generate_bounds.position.y, generate_bounds.end.y):
+        self._generate_row(j, generate_bounds.position.x, generate_bounds.end.x)
+
+func _shift(new_bounds, new_uncontained_bounds):
+    super(new_bounds, new_uncontained_bounds)
+    var new_generate_bounds = self._get_generate_bounds(new_uncontained_bounds)
+    if new_generate_bounds.has_area():
+        if !self._generate_bounds.has_area():
+            self._run_generate_on_bounds(new_uncontained_bounds)
+        else:
+            for j in self._get_new_top_row_indexes(self._generate_bounds,new_generate_bounds):
+                self._generate_row(j, new_generate_bounds.position.x,new_generate_bounds.end.x)
+            for j in self._get_new_bottom_row_indexes(self._generate_bounds, new_generate_bounds):
+                self._generate_row(j, new_generate_bounds.position.x, new_generate_bounds.end.x)
+            for i in self._get_new_left_col_indexes(self._generate_bounds, new_generate_bounds):
+                self._generate_col(i, new_generate_bounds.position.y, new_generate_bounds.end.y)
+            for i in self._get_new_right_col_indexes(self._generate_bounds, new_generate_bounds):
+                self._generate_col(i, new_generate_bounds.position.y,new_generate_bounds.end.y)
+    self._generate_bounds = new_generate_bounds
+
+func _clear_grid():
+    self._generate_bounds = Rect2i(0, 0, 0, 0)
+
+func _generate_row(j, start_i, end_i):
+    var neighboring_generators = self._get_neighboring_generators(start_i, j)
+    for i in range(start_i, end_i):
+        neighboring_generators[1][1].generate(neighboring_generators)
+        if i + 1 < end_i:
+            self._shift_neighboring_generators_right(neighboring_generators, i + 1, j)
+
+func _generate_col(i, start_j, end_j):
+    var neighboring_generators = self._get_neighboring_generators(i, start_j)
+    for j in range(start_j, end_j):
+        neighboring_generators[1][1].generate(neighboring_generators)
+        if j + 1 < end_j:
+            self._shift_neighboring_generators_down(neighboring_generators, i, j + 1)
+
+func _get_neighboring_generators(i, j):
+    var neighboring_generators = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ]
+    var neighbor_rect = Rect2i(i - 1, j - 1, 3, 3)
+    var contained_neighbor_rect = self._max_bounds.intersection(neighbor_rect)
+    for sub_i in range(contained_neighbor_rect.position.x, contained_neighbor_rect.end.x):
+        var neighbor_i = sub_i - i + 1
+        for sub_j in range(contained_neighbor_rect.position.y, contained_neighbor_rect.end.y):
+            var neighbor_j = sub_j - j + 1
+            neighboring_generators[neighbor_i][neighbor_j] = self.get_at(sub_i, sub_j)
+    return neighboring_generators
+
+func _shift_neighboring_generators_right(neighboring_generators, i, j):
+    neighboring_generators.pop_front()
+    neighboring_generators.push_back([null, null, null])
+    if i + 1 < self._max_bounds.end.x:
+        for sub_j in range(max(j - 1, self._max_bounds.position.y), min(j + 2, self._max_bounds.end.y)):
+            var neighbor_j = sub_j - j + 1
+            neighboring_generators[2][neighbor_j] = self.get_at(i + 1, sub_j)
+
+func _shift_neighboring_generators_down(neighboring_generators, i, j):
+    for neighbor_i in 3:
+        neighboring_generators[neighbor_i].pop_front()
+        neighboring_generators[neighbor_i].push_back(null)
+    if j + 1 < self._max_bounds.end.y:
+        for sub_i in range(max(i - 1, self._max_bounds.position.x), min(i + 2, self._max_bounds.end.x)):
+            var neighbor_i = sub_i - i + 1
+            neighboring_generators[neighbor_i][2] = self.get_at(sub_i, j + 1)
+
+func _get_generate_bounds(uncontained_bounds: Rect2i):
+    var uncontained_generate_bounds = self._get_rect_smaller_by(uncontained_bounds)
+    return self._max_bounds.intersection(uncontained_generate_bounds)
+
+func _get_rect_smaller_by(rect: Rect2i):
+    return Rect2i(
+        rect.position.x + self._generate_at_distance_from_x_sides,
+        rect.position.y + self._generate_at_distance_from_y_sides,
+        rect.size.x - self._generate_at_distance_from_x_sides * 2,
+        rect.size.y - self._generate_at_distance_from_y_sides * 2
+    )
