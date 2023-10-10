86b6ce248ad8f5d8c7fd7e1b6aba03abe48fbce3
Jester565 <ajcraig543@gmail.com>
2023-10-09 20:56:10 -0700

    more circular buffer methods

--- structures/CircularBuffer.gd
+++ structures/CircularBuffer.gd
@@ -22,11 +22,22 @@ func get_at(i):
     var arr_i = self._get_arr_i(i)
     return self._arr[arr_i]
 
+func for_each(callable: Callable):
+    if self._size > 0:
+        for arr_i in range(self._buffer_start_i, self._buffer_end_i + 1 if self._buffer_end_i >= self._buffer_start_i else self._arr.size()):
+            callable.call(self._arr[arr_i], arr_i + self._start_i - self._buffer_start_i)
+        if self._buffer_end_i < self._buffer_start_i:
+            for arr_i in range(0, self._buffer_end_i + 1):
+                callable.call(self._arr[arr_i], self._end_i - self._buffer_end_i + arr_i)
+
 func get_start_i():
     return self._start_i
 
 func get_end_i():
     return self._end_i
+
+func get_size():
+    return self._size
 
 func push_to_end(elm):
     var removed_elm = null
@@ -60,6 +71,28 @@ func push_to_start(elm):
     if self._buffer_start_i < 0:
         self._buffer_start_i = self._arr.size() - 1
     self._arr[self._buffer_start_i] = elm
+    return removed_elm
+
+func pop_from_start():
+    assert(self._size > 0, "cannot shrink when size is already 0")
+    var removed_elm = self._arr[self._buffer_start_i]
+    self._arr[self._buffer_start_i] = null
+    self._size -= 1
+    self._start_i += 1
+    self._buffer_start_i += 1
+    if self._buffer_start_i >= self._arr.size():
+        self._buffer_start_i = 0
+    return removed_elm
+
+func pop_from_end():
+    assert(self._size > 0, "cannot shrink when size is already 0")
+    var removed_elm = self._arr[self._buffer_end_i]
+    self._arr[self._buffer_end_i] = null
+    self._size -= 1
+    self._end_i -= 1
+    self._buffer_end_i -= 1
+    if self._buffer_end_i < 0:
+        self._buffer_end_i = self._arr.size() - 1
     return removed_elm
 
 func _get_arr_i(i):
