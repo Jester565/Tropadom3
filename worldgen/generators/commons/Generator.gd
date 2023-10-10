922bb48316a98321d900f1e914c08a2de560a3d3
Jester565 <ajcraig543@gmail.com>
2023-10-09 20:56:45 -0700

    add generator placeholder

---
+++ worldgen/generators/commons/Generator.gd
@@ -0,0 +1,18 @@
+extends GridElement
+
+class_name Generator
+
+# static func GetChunkWidth():
+#     return 8
+
+# static func GetChunkHeight():
+#     return 8
+
+func generate(neighboaring_generators):
+    pass
+
+func get_elements_intersecting_with(bounding_rect: Rect2i):
+    return []
+
+func get_elements():
+    pass
